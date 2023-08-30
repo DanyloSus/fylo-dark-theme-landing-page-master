7b6470a7a1944aed29186e3e4f3852bdc8e7ea73
Danylo Sus <danil.sushko19@gmail.com>
2023-08-30 17:58:58 +0300

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss')",.Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "intro-and-email-sign-up-background": "hsl(217, 28%, 15%)",
+        "main-background": "hsl(218, 28%, 13%)",
+        "footer-background": "hsl(216, 53%, 9%)",
+        "testimonials-background": "hsl(219, 30%, 18%)",
+
+        cyan: "hsl(176, 68%, 64%)",
+        blue: "hsl(198, 60%, 50%)",
+        error: "hsl(0, 100%, 63%)",
+
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
